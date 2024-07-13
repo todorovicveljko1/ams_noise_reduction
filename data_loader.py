ed1ab54f039e154dfba3842a96f07d9188112521
Veljko Todorovic <todorovicveljko1@gmail.com>
2024-07-13 16:11:16 +0200

    added colab used to train models

--- data_loader.py
+++ data_loader.py
@@ -4,7 +4,8 @@ import numpy as np
 import os
 import random
 from PIL import Image
-import torchvision.transforms as transforms
+from torchvision.transforms import v2
+
 
 
 class CustomDataset(data.Dataset):
@@ -29,17 +30,22 @@ class CustomDataset(data.Dataset):
         :param index: index of image
         :return: image, noisy image, noise mask
         """
-        img = Image.open(self.imgs[index])
-        if self.transform is not None:
-            img = self.transform(img)
+        img = np.array(Image.open(self.imgs[index]), np.float32)
         img = (img - img.min()) / (img.max() - img.min())
-        noise = self.noise_gen.noise(img.shape[1], img.shape[2])
+        x,y = img.shape[0], img.shape[1]
+        noise = self.noise_gen.noise(x,y)
         noisy_img = img + noise
-        noisy_img = torch.clamp(noisy_img, 0.0, 1.0)
-
+        if self.transform is not None:
+            img, noisy_img, noise = self.transform(img, noisy_img, noise)
+            #noisy_img = self.transform(noisy_img) #???
+           # noise = self.transform(noise)
         # to device
         img = img.to(self.device)
+
+        # mask = torch.bernoulli(torch.full((x,y), 0.5)) # 50% -> 0 or 1
+        # noisy_img = noisy_img*mask
         noisy_img = noisy_img.to(self.device)
+
         noise = noise.to(self.device)
 
         return img, noisy_img, noise
@@ -50,13 +56,13 @@ class CustomDataset(data.Dataset):
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
     from noise_gen import GaussianNoiseGen, CompositeNoise, PepperSaltNoiseGen, PoissonNoiseGen
-    from torchvision import transforms
-    noise_gen = CompositeNoise()
-    noise_gen.add(GaussianNoiseGen(0, 0.2), 0, 0.3, 1, 1, -100, 255)
-    noise_gen.add(PoissonNoiseGen(2), 0, 0.1, 1, 0.3, -100, 255)
-    noise_gen.add(PepperSaltNoiseGen(0.001), 0, 0.5, 1, 1, 0, 255)
+
+    noise_gen = CompositeNoise(scale=0.1, value_prob=0.5)
+    noise_gen.add(GaussianNoiseGen(0, 0.2), scale=0.5, min_clip=-1.0)
+    noise_gen.add(PoissonNoiseGen(2), scale = 0.1)
+    noise_gen.add(PepperSaltNoiseGen(0.005), scale = 0.5)
     # scale to [0, 1]
-    trans = transforms.Compose([transforms.ToTensor()])
+    trans = v2.Compose([v2.ToTensor()])
     dataset = CustomDataset('data\\dataset\\train', noise_gen, trans)
     img, noisy_img, noise = dataset[0]
     # show img, noise and noise mask next to each other
@@ -64,7 +70,7 @@ if __name__ == '__main__':
     plt.subplot(1,3,1)
     plt.imshow(img.numpy().transpose(1,2,0))
     plt.subplot(1,3,2)
-    plt.imshow(noise.numpy())
+    plt.imshow(noise.numpy().transpose(1,2,0))
     plt.subplot(1,3,3)
     plt.imshow(noisy_img.numpy().transpose(1,2,0))
     plt.show()
--- noise_gen.py
+++ noise_gen.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
+import os
 import numpy as np
-import torch
+
 class NoiseGen(ABC):
     """
     Abstract class for noise generator
@@ -17,13 +18,13 @@ class NoiseGen(ABC):
 
 class PoissonNoiseGen(NoiseGen):
     """
-    Poisson noise generator
+    Poisson noise generator note that this generate integer noise >= 0, so it needs to be scaled   
     """
     def __init__(self, lam):
         self.lam = lam 
 
     def noise(self, x, y):
-        return torch.poisson(torch.ones(1) * self.lam)
+        return np.random.poisson(self.lam, (x, y))
 
 class PepperSaltNoiseGen(NoiseGen):
     """
@@ -33,7 +34,7 @@ class PepperSaltNoiseGen(NoiseGen):
         self.p = p
 
     def noise(self, x, y):
-        return torch.bernoulli(torch.ones(x, y) * self.p)
+        return np.random.binomial(1, self.p, (x, y))
 
 class GaussianNoiseGen(NoiseGen):
     """
@@ -44,17 +45,18 @@ class GaussianNoiseGen(NoiseGen):
         self.std = std
 
     def noise(self, x, y):
-        return torch.normal(self.mean, self.std, (x, y))
+        return np.random.normal(self.mean, self.std, (x, y)) 
 
 class CompositeNoise(NoiseGen):
     """
     Composite noise generator
     """
-    def __init__(self, min_clip = 0., max_clip = 1., scale=1.) -> None:
+    def __init__(self, min_clip = 0.0, max_clip = 1.0, scale=1.0, value_prob=1.0) -> None:
         self.noises = []
         self.min_clip = min_clip
         self.max_clip = max_clip
         self.scale = scale
+        self.value_prob = value_prob
 
     def add(self, noise, offset=0.0, scale=1.0, prob=1.0, value_prob=1.0, min_clip=0.0, max_clip=1.0):
         """
@@ -78,24 +80,68 @@ class CompositeNoise(NoiseGen):
         })
     
     def noise(self, x, y):
-        noise = torch.zeros((x, y))
+        noise = np.zeros((x, y), np.float32)
         for n in self.noises:
-            if np.random.rand() < n['prob']:
-                temp_noise = n['noise'].noise(x, y) * n['scale'] + n['offset']
-                if n['value_prob'] < 1.0:
-                    temp_noise = np.where(np.random.rand(x, y) < n['value_prob'], temp_noise, 0.0)
-                noise += np.clip(temp_noise , n['min_clip'], n['max_clip'])
+            if np.random.rand() > n['prob']: # skip this noise
+                continue
+            temp_noise = n['noise'].noise(x, y) * n['scale'] + n['offset']
+            if n['value_prob'] < 1.0:
+                temp_noise = np.where(np.random.rand(x, y) < n['value_prob'], temp_noise, 0.0)
+            noise += np.clip(temp_noise , n['min_clip'], n['max_clip'])
+        if self.value_prob < 1.0:
+            noise = np.where(np.random.rand(x, y) < self.value_prob, noise, 0.0)
         return np.clip(self.scale*noise, self.min_clip, self.max_clip)
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
-    noise_gen = CompositeNoise()
-    noise_gen.add(GaussianNoiseGen(0, 0.1), 0, 1, 1, 1, -100, 255)
-    noise_gen.add(PoissonNoiseGen(1), 0, 1/12, 1, 0.3, -100, 255)
-    noise_gen.add(PepperSaltNoiseGen(0.01), 0, 0.7, 1, 1, 0, 255)
+    from PIL import Image
+
+    noise_gen = CompositeNoise(scale=0.1, value_prob=0.5)
+    noise_gen.add(GaussianNoiseGen(0, 0.2), scale=0.5, min_clip=-1.0)
+    noise_gen.add(PoissonNoiseGen(2), scale = 0.1)
+    noise_gen.add(PepperSaltNoiseGen(0.005), scale = 0.5)
+    
     noise = noise_gen.noise(224, 224)
-    print(noise)
-    plt.imshow(noise.numpy(), cmap='gray')
+    plt.imshow(noise, cmap='gray')
     plt.show()
-    plt.hist(noise.numpy().flatten(), bins=100)
+    plt.hist(noise.flatten(), bins=100)
     plt.show()
+    # Check if file exists
+    if not os.path.exists('temp/example.png'):
+        exit()
+
+    img = Image.open('temp/example.png')
+    
+    #trans = transforms.Compose([transforms.ToTensor()])
+    #img = trans(img)
+    img = np.array(img)
+    img = (img - img.min()) / (img.max() - img.min())
+    # img crop to multiple of 32
+    img = img[:(img.shape[0] - img.shape[0]%32), :(img.shape[1] - img.shape[1]%32)]
+    #print(img.shape)
+    # add noise
+
+    noise = noise_gen.noise(img.shape[0], img.shape[1])
+
+    noisey_img = img + noise
+
+    print('img std', img.std(), 'mean', img.mean())
+    print('noisey_img std', noisey_img.std(), 'mean', noisey_img.mean())
+    print('noise std', noise.std(), 'mean', noise.mean())
+
+    # show img, noisey_img
+    plt.figure()
+
+    plt.subplot(1,3,1)
+    plt.imshow(img, cmap='gray')
+
+    plt.subplot(1,3,2)
+    plt.imshow(noisey_img, cmap='gray')
+
+    plt.subplot(1,3,3)
+    plt.imshow(noise, cmap='gray')
+
+    plt.show()
+
+
+
